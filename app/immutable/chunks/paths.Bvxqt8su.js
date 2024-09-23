2ed93e76238d08fa865060eaad48999842ac27b4
TombNS <tom@nutriseed.co.uk>
2024-09-23 22:08:06 +0100

    Build #5
    Hopefully finally working.

--- app/immutable/chunks/paths.Bvxqt8su.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1dpvwmq)==null?void 0:s.base)??"/find-the-spy";var e;const a=((e=globalThis.__sveltekit_1dpvwmq)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ app/immutable/chunks/paths.DEpU_XMi.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1lavniv)==null?void 0:s.base)??"/find-the-spy";var a;const t=((a=globalThis.__sveltekit_1lavniv)==null?void 0:a.assets)??e;export{t as a,e as b};
