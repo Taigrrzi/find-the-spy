82d3b0340bc2e9d08ae2f552462b2476ccf87362
TombNS <tom@nutriseed.co.uk>
2024-09-26 17:02:31 +0100

    Fixed Timer Bug
    On ios, timer would not count down when device was locked.

--- app/immutable/chunks/paths.BU2HZKYj.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1rcn57a)==null?void 0:s.base)??"/find-the-spy";var a;const t=((a=globalThis.__sveltekit_1rcn57a)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ app/immutable/entry/start.BNf59UnY.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Br0pbAV1.js";export{t as start};
